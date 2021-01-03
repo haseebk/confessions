1d1c8dbb2ebbfb053968e990fba3710033ef06f4
Haseeb <haseebadilkhan@gmail.com>
2021-01-02 22:41:06 -0700

    deploying to heroku

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // jshint esversion:6
 
 require("dotenv").config();
-const express = require("express");
 const ejs = require("ejs");
+const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const session = require("express-session");
